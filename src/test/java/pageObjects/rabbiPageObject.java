5ed56ab7ea255ea879550a81e6de368494e2fdd1
RabbiShakir <rabbishakir@gmail.com>
2025-04-20 13:04:52 -0400

    Added a new file

---
+++ src/test/java/pageObjects/rabbiPageObject.java
@@ -0,0 +1,14 @@
+package pageObjects;
+
+public class Ahadpageobject {
+
+	
+	public void runner() {
+		System.out.println("somthing");
+	}
+	
+	public void runtwo() {
+		
+		System.out.println("nothing");
+	}
+}
